bbd8bcd01a9770f158a6807cf1b1f4d538adb01c
Antoine Cantoro <antoine.cantoro@gmail.com>
2026-01-22 23:02:51 +0100

    FEAT: setup bun and typescript

---
+++ index.ts
@@ -0,0 +1,13 @@
+import glsl from 'bun-plugin-glsl';
+import index from './src/index.html';
+
+console.log("Hello via Bun!");
+Bun.plugin(glsl());
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": index,
+  }
+});
+
+console.log(`Listening on ${server.url}`);
---
+++ src/shaders/base/fragment.js
@@ -0,0 +1,11 @@
+export default /* glsl */`
+precision highp float;
+
+uniform float uTime;
+
+varying vec2 vUv;
+
+void main() {
+    gl_FragColor.rgb = vec3(0.8, 0.7, 1.0) + 0.3 * cos(vUv.xyx + uTime);
+    gl_FragColor.a = 1.0;
+}`
